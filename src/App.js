8ad2ac41ad77498904ece7fd3954aed5fde3dd76
Alton Michaux <alton.michaux@yahoo.com>
2022-12-22 00:29:02 -0500

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { ApiFetch } from './api-fetch';
 import { FetchApiList } from './utils';
+import './App.css'
 
 function App() {
   return (
-    <div className="main-div">
-      {FetchApiList.map((list) => {
-        return (
-          <ApiFetch
-            id={list.key}
-            title={list.title}
-            fetchUrl={list.url}
-          />
-        )
-      })
-      }
-    </div>
+    <>
+      <h1>Free APIs</h1>
+      <div className="main-div">
+        {FetchApiList.map((list) => {
+          return (
+            <ApiFetch
+              id={list.key}
+              title={list.title}
+              fetchUrl={list.url}
+            />
+          )
+        })
+        }
+      </div>
+    </>
   );
 }
 
--- src/api-fetch.jsx
+++ src/api-fetch.jsx
@@ -2,19 +2,29 @@ import React, { useState } from 'react'
 
 export const ApiFetch = ({ id, title, fetchUrl }) => {
     const [fetchResponse, setFetchResponse] = useState(null)
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleFetch = async () => {
+        setIsLoading(true)
         const res = await fetch(fetchUrl);
         const data = await res.json();
         const jsonPretty = JSON.stringify(data, null, 2)
+        setIsLoading(false)
         setFetchResponse(jsonPretty)
     }
 
     return (
         <div className="api-div" id={id}>
-            <h1>{title}</h1>
-            <button onClick={() => { handleFetch() }}>Fetch</button>
-            <pre>{fetchResponse}</pre>
+            <h2>{title}</h2>
+            {isLoading ?
+                <>
+                    <p>Loading...</p>
+                </> :
+                <>
+                    <button onClick={() => { handleFetch() }}>Fetch</button>
+                    <pre>{fetchResponse}</pre>
+                </>
+            }
         </div>
     )
 }
